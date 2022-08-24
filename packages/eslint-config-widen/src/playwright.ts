bfadc47b2a9ec59ae01844a7483bca6115986910
Mark Skelton <mark.skelton@acquia.com>
2022-08-24 11:51:10 -0500

    Update plugin version (#22)
    * Enable rules
    
    * Lockfile

--- packages/eslint-config-widen/src/playwright.ts
+++ packages/eslint-config-widen/src/playwright.ts
@@ -4,12 +4,12 @@ export = {
       extends: ['plugin:playwright/playwright-test'],
       plugins: ['playwright'],
       rules: {
-        // TODO: Uncomment when these rules are released
-        // 'playwright/prefer-lowercase-title': [
-        //   'warn',
-        //   { ignoreTopLevelDescribe: true },
-        // ],
-        // 'playwright/require-top-level-describe': 'warn',
+        'playwright/prefer-lowercase-title': [
+          'warn',
+          { ignoreTopLevelDescribe: true },
+        ],
+        'playwright/prefer-to-have-length': 'warn',
+        'playwright/require-top-level-describe': 'warn',
       },
     },
   ],
