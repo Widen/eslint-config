b2124a023da07977da23b3e23b15fae82af6dcb7
Joseph Phelan <jphelan@widen.com>
2024-07-11 11:09:42 -0500

    cleanup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,6 +8,8 @@ import { fileURLToPath } from 'node:url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+// This is in place until https://github.com/typescript-eslint/typescript-eslint
+// updates to v8
 const compat = new FlatCompat({
   allConfig: js.configs.all,
   baseDirectory: __dirname,
