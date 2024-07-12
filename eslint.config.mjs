cf276434ad4884964bd6aba937cd3ec93e1c4eee
Joseph Phelan <jphelan@widen.com>
2024-07-12 14:35:23 -0500

    update project typescript-eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,17 @@
-import { FlatCompat } from '@eslint/eslintrc'
-import js from '@eslint/js'
+import eslint from '@eslint/js'
 import typescriptEslint from '@typescript-eslint/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
+import sort from 'eslint-plugin-sort'
 import globals from 'globals'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-//TODO: cleanup compat https://acquia.atlassian.net/browse/FE-1172
-const compat = new FlatCompat({
-  allConfig: js.configs.all,
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-})
+import tseslint from 'typescript-eslint'
 
 export default [
   {
     ignores: ['**/lib/', '**/packages/eslint-playground/'],
   },
-  ...compat.extends(
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:sort/recommended',
-  ),
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  sort.configs['flat/recommended'],
   {
     languageOptions: {
       globals: {
